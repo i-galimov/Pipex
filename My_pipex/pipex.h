dfefd19f56bc64f83e25b8ac7e5142dfe87ee396
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2022-01-13 23:18:20 +0300

    Add files via upload

---
+++ My_pipex/pipex.h
@@ -0,0 +1,19 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include   <unistd.h>
+# include   <stdlib.h>
+# include   <fcntl.h>
+# include   <stdio.h>
+
+char	**ft_split(const char *s, char c);
+int     ft_strlen(const char *str);
+char	*ft_strnstr(const char *haystack, const char *needle, int len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_path(char *cmd, char **env);
+void	ft_get_cmd(char *argv, char **env);
+void    ft_parent_process(char **argv, char **env, int *fd);
+void    ft_child_process(char **argv, char **env, int *fd);
+int		main(int argc, char **argv, char **env);
+
+#endif
